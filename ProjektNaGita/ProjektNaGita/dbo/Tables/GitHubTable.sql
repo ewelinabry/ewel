77140fd1aaef72d4ba512060042c6302aaa2e74d
e.brys <e.brys@PK5-09>
2019-07-29 18:52:10 +0200

    Załadowanie projektu

---
+++ ProjektNaGita/ProjektNaGita/dbo/Tables/GitHubTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[GitHubTable] (
+    [kolumna01] INT            IDENTITY (1, 1) NOT NULL,
+    [kolumna02] NVARCHAR (100) NULL,
+    [kolumna03] NVARCHAR (100) NULL,
+    PRIMARY KEY CLUSTERED ([kolumna01] ASC)
+);
+
